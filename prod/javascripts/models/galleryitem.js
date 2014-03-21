d4e5b3a742ec2547721c0d3193e92f00cbc1b09b
Jean-Louis Danielo <jeanlouis.danielo@sfr.fr>
2014-03-22 00:49:58 +0100

    remove prod folder

--- prod/javascripts/models/galleryitem.js
+++
@@ -1,7 +0,0 @@
-define(['backbone'], function() {
-    window.portfolio.galleryItem = Backbone.Model.extend({
-        urlRoot: 'javascripts/datas.json'
-    });
-
-    return window.portfolio.galleryItem;
-});
