d42d26f6cdf70d88bc592e63cbf172d63870f724
Jean-Louis Danielo <jeanlouis.danielo@sfr.fr>
2014-03-24 22:57:52 +0100

    refactoring

--- dev/javascripts/views/galleryView.js
+++ dev/javascripts/views/galleryView.js
@@ -16,28 +16,14 @@
         var $containerForeground = $(this.el).find('div.foreground'),
             $containerBackground = $(this.el).find('div.background');
 
-        // var $imgForeground = $containerForeground.find('img.foreground'),
-        //     $imgBackground = $containerBackground.find('img.background');
-
           $containerForeground.addClass('foreground-anim');
 
           loader.addProgressListener(function(e) {
-            console.log(e.resource);
-            $containerBackground.html(e.resource.img);
-            $containerBackground.addClass('background-anim');
-
-            //e.resource.img.attr('data-id', _that.model.toJSON().id);
-            // $imgBackground.attr('src', _that.model.toJSON().imgSRC)
-            //               .attr('data-id', _that.model.toJSON().id)
-            //               .addClass('background-anim');
+            $containerBackground.html(e.resource.img).addClass('background-anim');
+            $containerBackground.find('img').img.attr('data-id', _that.model.toJSON().id);
           });
 
-           //$('ul.gallery li').eq(id).addClass('active');
-
-          // $(document).ready(function(){
-            $('ul.gallery li').eq(id).addClass('active');
-          //});
-
+          $('ul.gallery li').eq(id).addClass('active');
 
           loader.start();
 
